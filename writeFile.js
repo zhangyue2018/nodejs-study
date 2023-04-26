0e026a582a4da19a6c4c7285df61e3b9ef72853f
zhangyue <zhangyuea@hualala.com>
2023-04-26 17:02:34 +0800

    文件追加写入

--- writeFile.js
+++ writeFile.js
@@ -14,4 +14,26 @@ fs.writeFile('./座右铭.txt', '三人行，则必有我师焉', err => {
 });
 
 // 同步写入
-fs.writeFileSync('./data.txt', 'test data');
+// fs.writeFileSync('./data.txt', 'test data');
+
+// 异步追加写入
+fs.appendFile('./座右铭.txt', '，择其善者而从之，其不善者而改之。', err => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('异步追加写入文件，成功');
+});
+
+// 同步追加写入文件
+fs.appendFileSync('./data.txt', '\r\n温故而知新，可以为师矣');
+
+
+// writeFile实现追加写入
+fs.writeFile('./座右铭.txt', '\r\n良禽择木而栖，识时务者为俊杰', {flag: 'a'}, err => {
+    if(err) {
+        console.log('追加失败：', err);
+        return;
+    }
+    console.log('追加写入文件成功---writeFile');
+});
