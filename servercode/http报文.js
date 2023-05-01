61e85e54af85c6791a404e3d8b2482ed1bb364a2
ZY <1822545638@qq.com>
2023-05-01 14:57:59 +0800

    '获取请求路径与查询字符串-新'

--- servercode/http报文.js
+++ servercode/http报文.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const url = require('url');
 // 创建服务对象
 const server = http.createServer((request, response) => {
     // // 获取请求的方法
@@ -21,9 +20,16 @@ const server = http.createServer((request, response) => {
     // });
 
     // 获取请求路径和查询字符串
-    let res = url.parse(request.url, true); // parse第二个参数，如果为true，则res.query被解析成对象,默认为false
-    console.log(res);
-    response.end(res.path);
+    // let res = url.parse(request.url, true); // parse第二个参数，如果为true，则res.query被解析成对象,默认为false
+    // console.log(res);
+    // response.end(res.path);
+
+    // 获取查询路径和查询字符串的另外一种方式
+    // new URL(path[, base]); // 如果path是完整的url（包含协议域名），则不用传base参数
+    let url = new URL('/a?search=12345', 'http://127.0.0.1:8080');
+    console.log(url);
+    console.log(url.searchParams.get('search'));
+    response.end('url new');
 });
 
 // 监听端口，启动服务
