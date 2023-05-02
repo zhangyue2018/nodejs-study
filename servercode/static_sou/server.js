b09ec882b135320e1784bfbfd2dad074e5a08499
ZY <1822545638@qq.com>
2023-05-02 21:46:07 +0800

    设置mime类型

--- servercode/static_sou/server.js
+++ servercode/static_sou/server.js
@@ -6,6 +6,19 @@
  */
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+// 声明一个变量
+const mimes = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'jpg': 'image/jpeg',
+    'js': 'text/javascript',
+    'png': 'image/png',
+    'gif': 'image/gif',
+    'mp4': 'video/mp4',
+    'mp3': 'audio/mpeg',
+    'json': 'application/json'
+}
 
 const server = http.createServer((req, res) => {
     let {pathname} = new URL(req.url, 'http://127.0.0.1');
@@ -16,8 +29,18 @@ const server = http.createServer((req, res) => {
         console.log('读取文件成功');
         if(err) {
             res.statusCode = 500;
+            res.setHeader('content-type', 'text/html;charset=utf-8');
             res.end('没有找到');
             return;
+        }
+        const ext = path.extname(fileName).slice(1);
+        const type = mimes[ext];
+        if(type) {
+            res.setHeader('content-type', type);
+        } else {
+            // 对于未知的资源类型，可以选择'application/octet-stream'类型，浏览器在遇到该类型的响应时
+            // 会对响应体内容进行独立存储，也就是常见的瞎咋效果
+            res.setHeader('content-type', 'application/octet-stream');
         }
         res.end(data);
     });
