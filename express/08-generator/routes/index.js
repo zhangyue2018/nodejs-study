473cc426d340db0337ff44210c8d9a2c04a8608d
ZY <1822545638@qq.com>
2023-05-06 16:47:51 +0800

    处理文件上传

--- express/08-generator/routes/index.js
+++ express/08-generator/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var formidable = require('formidable');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
@@ -11,6 +13,22 @@ router.get('/portrait', function(req, res, next) {
 });
 
 router.post('/portrait', (req, res, next) => {
+    let form = formidable({
+        multiples: true,
+        // 设置上传文件的保存目录
+        uploadDir: path.resolve(__dirname, './../public/images'),
+        // 下面这种方式也可以
+        // uploadDir: __dirname + '/../public/images',
+        // 保持文件后缀
+        keepExtensions: true
+    });
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return next(err);
+        }
+        console.log(fields); // fields存储文件上传之外的字段：text radio checkbox select
+        console.log(files);  // 存储file类型的数据
+    });
     res.send('成功');
 });
 
