cf159ab23aa39a9a0e028c1dcb4375fc9f890971
zhangyue <zhangyuea@hualala.com>
2023-05-09 18:07:35 +0800

    账单列表功能

--- account/routes/index.js
+++ account/routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/account', function(req, res, next) {
-  res.render('list');
+
+    let accounts = [
+        {
+            id: '00001',
+            title: '发工资',
+            time: '2023-05-10',
+            type: '1',
+            account: '4396',
+            remarks: '终于发工资啦~'
+        },
+        {
+            id: '00002',
+            title: '买鼠标',
+            time: '2023-05-12',
+            type: '-1',
+            account: '4346',
+            remarks: '鼠标坏了，换一个新的'
+        },
+        {
+            id: '00003',
+            title: '吃饭',
+            time: '2023-05-15',
+            type: '-1',
+            account: '4316',
+            remarks: ''
+        }
+    ];
+  res.render('list', {accounts});
 });
 
 router.get('/account/create', function(req, res, next) {
