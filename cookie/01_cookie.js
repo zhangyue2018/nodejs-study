0282f27afe4cd98005dadee291ddd8882213c19e
ZY <1822545638@qq.com>
2023-05-11 01:19:31 +0800

    express框架中获取cookie

--- cookie/01_cookie.js
+++ cookie/01_cookie.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 
 app.get('/set-cookie', (req, res) => {
     // res.cookie('name', 'zzyy');  // 会在浏览器关闭的时候销毁
@@ -12,6 +14,13 @@ app.get('/set-cookie', (req, res) => {
 app.get('/remove-cookie', (req, res) => {
     res.clearCookie('name');
     res.send('删除成功~~');
+});
+
+// 获取cookie
+app.get('/get-cookie', (req, res) => {
+    console.log(req.cookies);
+    res.send(`欢迎你，${req.cookies.name}`);
+
 });
 
 app.listen(3000, () => {
