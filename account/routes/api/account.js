22e1d8c891a5465fc66088d04ffa449319c92d8c
zhangyue <zhangyuea@hualala.com>
2023-05-10 19:04:05 +0800

    记账本-添加增删改查api接口

--- account/routes/api/account.js
+++ account/routes/api/account.js
@@ -3,43 +3,106 @@ const moment = require('moment');
 const AccountModel = require('../../models/AccountModel');
 var router = express.Router();
 
-/* GET home page. */
+// 记账本的列表
 router.get('/account', function(req, res, next) {
     AccountModel.find().then(data => {
-        console.log('---data----', data);
-
-        res.render('list', {accounts: data, moment});
+        res.json({
+            code: '0000',
+            msg: '读取成功',
+            data
+        });
     }, err => {
-        res.status(500).send('读取数据失败!');
+        res.json({
+           code: '1001',
+           msg: '读取失败',
+           data: null
+        });
     });
   
 });
-
-router.get('/account/create', function(req, res, next) {
-    res.render('create');
-});
-
-router.get('/account/:id', (req, res) => {
+// 删除记录
+router.delete('/account/:id', (req, res) => {
     let id = req.params.id;
 
     AccountModel.deleteOne({_id: id}).then(data => {
-        res.render('success', {msg: '删除成功', url: '/account'});
+        res.json({
+            code: '0000',
+            msg: '删除账单成功',
+            data: {}
+        });
     }, err => {
-        res.status(500).send('删除失败!!');
+        res.json({
+            code: '1003',
+            msg: '删除账单失败',
+            data: null
+        });
     });
 });
-
+// 新增记录
 router.post('/account', (req, res) => {
     console.log('req.body---', req.body);
     AccountModel.create({
         ...req.body,
         time: moment(req.body.time).toDate()
-    }).then(() => {
-        res.render('success', {msg: '添加成功', url: '/account'});
+    }).then((data) => {
+        res.json({
+            code: '0000',
+            msg: '创建成功',
+            data
+        });
     }, err => {
         res.status(500).render('插入失败~~~');
+        res.json({
+            code: '1002',
+            msg: '创建失败',
+            data: null
+        });
     });
-    
+});
+// 获取单个账单信息
+router.get('/account/:id', (req, res) => {
+    let id = req.params.id;
+    AccountModel.findById({_id: id}).then(data => {
+        res.json({
+            code: '0000',
+            msg: '读取单条数据成功',
+            data
+        });
+    }, err => {
+        res.json({
+            code: '1004',
+            msg: '读取单条数据失败',
+            data: null
+        });
+    });
+});
+
+// 更新单个账单信息
+router.patch('/account/:id', (req, res) => {
+    let id = req.params.id;
+    AccountModel.updateOne({_id: id}, req.body)
+        .then(data => {
+            return AccountModel.findById({_id: id})
+        }, err => {
+            res.json({
+                code: '1005',
+                msg: '更新账单信息失败',
+                data: null
+            });
+        })
+        .then(data => {
+            res.json({
+                code: '0000',
+                msg: '更新账单信息成功',
+                data
+            });
+        }, err => {
+            res.json({
+                code: '1004',
+                msg: '读取单条数据失败',
+                data: null
+            });
+        });
 });
 
 module.exports = router;
