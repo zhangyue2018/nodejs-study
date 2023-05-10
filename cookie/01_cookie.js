afe0e18b2565b9691c768541e7688326303f4631
ZY <1822545638@qq.com>
2023-05-11 01:10:19 +0800

    express框架中删除cookie

--- cookie/01_cookie.js
+++ cookie/01_cookie.js
@@ -5,7 +5,13 @@ const app = express();
 app.get('/set-cookie', (req, res) => {
     // res.cookie('name', 'zzyy');  // 会在浏览器关闭的时候销毁
     res.cookie('name', 'lisi', { maxAge: 60 * 1000 }); // maxAge单位是ms
+    res.cookie('theme', 'blue', { maxAge: 60 * 1000 });
     res.send('hello world');
+});
+
+app.get('/remove-cookie', (req, res) => {
+    res.clearCookie('name');
+    res.send('删除成功~~');
 });
 
 app.listen(3000, () => {
