bc6d4bee31d5162495bb83ea17512b8af95c1259
zhangyue <zhangyuea@hualala.com>
2023-05-11 16:26:27 +0800

    express中session操作--session的设置,读取，销毁

--- cookie/02_session.js
+++ cookie/02_session.js
@@ -22,6 +22,37 @@ app.use(session({
 app.get('/', (req, res) => {
     res.send('home');
 });
+
+// 登录
+app.get('/login', (req, res) => {
+    // username=admin&password=admin
+    if(req.query.username === 'admin' && req.query.password === 'admin') {
+        // 设置session信息
+        req.session.username = 'admin';
+        // 成功响应
+        res.send('登陆成功');
+    } else {
+        res.send('登陆失败~~~');
+    }
+});
+
+// session读取
+app.get('/cart', (req, res) => {
+    // 检测session是否存在用户数据
+    if(req.session.username) {  // 中间件已经对session进行处理了
+        res.send(`购物车页面，欢迎您${req.session.username}`);
+    } else {
+        res.send('您还没有登录~~~');
+    }
+});
+
+// sesiion销毁
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.send('退出成功~~~');
+    });
+});
+
 app.listen(3000, () => {
     console.log('服务已经启动~');
 });
